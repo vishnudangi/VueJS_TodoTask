5f8ec5ebcf5ff2d199e748f256b4965a05a1d24c
Vishnu Dangi <vishnu.dangi@styletag.com>
2017-08-02 00:44:36 +0530

    todo task Add, Edit and Delete in VueJs

---
+++ app.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+  el: '#root',
+  data : {
+   message : "to do App",
+    todos: [],
+    inputTask: '',
+    isEditTask : false,
+    editIndex : ''
+    },
+  methods : {
+    addTask : function(){
+      if(!this.isEditTask){
+       this.todos.push(this.inputTask);
+      }
+      else {
+        this.todos.splice(this.editIndex, 1, this.inputTask)
+      }
+      this.isEditTask = false;
+      this.inputTask = '';
+    },
+    editTask : function(index){
+      this.inputTask = this.todos[index];
+      this.isEditTask = true;
+      this.editIndex = index;
+    },
+    
+    deleteTask : function(index){
+      this.todos.splice(index, 1);
+    }
+  }
+})
